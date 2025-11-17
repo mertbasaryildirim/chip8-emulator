872cf35096338a241bc88540c6884851d63b26b3
mertbasaryildirim <mertbasaryildirim@gmail.com>
2025-11-17 22:27:54 +0300

    implement OP_00EE

--- src/instructions.c
+++ src/instructions.c
@@ -5,3 +5,9 @@ void OP_00E0()
 {
     memset(chip8_memory.display, 0, sizeof(chip8_memory.display));
 }
+
+void OP_00EE()
+{
+    --chip8_memory.stack_pointer;
+    chip8_memory.program_counter = *(chip8_memory.stack + chip8_memory.stack_pointer);
+}
