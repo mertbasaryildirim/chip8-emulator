a6d289e0c462260e3c90d30db856da5fa13a123a
mertbasaryildirim <mertbasaryildirim@gmail.com>
2025-11-18 10:48:54 +0300

    fix array index pointer operations

--- src/instructions.c
+++ src/instructions.c
@@ -9,7 +9,7 @@ void OP_00E0()
 
 void OP_00EE()
 {
-    chip8_memory.program_counter = *(chip8_memory.stack + --chip8_memory.stack_pointer);
+    chip8_memory.program_counter = chip8_memory.stack[--chip8_memory.stack_pointer];
 }
 
 void OP_1nnn()
@@ -19,14 +19,14 @@ void OP_1nnn()
 
 void OP_2nnn()
 {
-    *(chip8_memory.stack + chip8_memory.stack_pointer++) = chip8_memory.program_counter;
+    chip8_memory.stack[chip8_memory.stack_pointer++] = chip8_memory.program_counter;
     chip8_memory.program_counter = opcode & 0x0FFFu;
 }
 
 void OP_3xkk()
 {
     uint8_t register_address = (opcode & 0x0F00u) >> 8u;
-    uint8_t register_value = *(chip8_memory.registers + register_address);
+    uint8_t register_value = chip8_memory.registers[register_address];
 
     if (register_value == (opcode & 0x00FFu))
         chip8_memory.program_counter += 2;
@@ -35,7 +35,7 @@ void OP_3xkk()
 void OP_4xkk()
 {
     uint8_t register_address = (opcode & 0x0F00u) >> 8u;
-    uint8_t register_value = *(chip8_memory.registers + register_address);
+    uint8_t register_value = chip8_memory.registers[register_address];
 
     if (register_value != (opcode & 0x00FFu))
         chip8_memory.program_counter += 2;
@@ -44,10 +44,10 @@ void OP_4xkk()
 void OP_5xy0()
 {
     uint8_t register_address_x = (opcode & 0x0F00u) >> 8u;
-    uint8_t register_value_x = *(chip8_memory.registers + register_address_x);
+    uint8_t register_value_x = chip8_memory.registers[register_address_x];
 
     uint8_t register_address_y = (opcode & 0x00F0u) >> 4u;
-    uint8_t register_value_y = *(chip8_memory.registers + register_address_y);
+    uint8_t register_value_y = chip8_memory.registers[register_address_y];
 
     if (register_value_x == register_value_y)
         chip8_memory.program_counter += 2;
@@ -57,7 +57,8 @@ void OP_6xkk()
 {
     uint8_t register_address = (opcode & 0x0F00u) >> 8u;
     uint8_t value = opcode & 0x00FFu;
-    *(chip8_memory.registers + register_address) = value;
+
+    chip8_memory.registers[register_address] = value;
 }
 
 void OP_7xkk()
@@ -65,5 +66,5 @@ void OP_7xkk()
     uint8_t register_address = (opcode & 0x0F00u) >> 8u;
     uint8_t value = opcode & 0x00FFu;
 
-    *(chip8_memory.registers + register_address) += value;
+    chip8_memory.registers[register_address] += value;
 }
